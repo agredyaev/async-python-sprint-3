6e63a76ff14ae96f42b5ac59cc2cafad28028e0e
Aleksey Gredyaev <44847900+agredyaev@users.noreply.github.com>
2025-02-01 11:12:35 +0200

    Revert "feat/add-async-server"

--- src/protocols/__init__.py
+++
@@ -1,3 +0,0 @@
-from src.protocols.repositories.repository import RepositoryProtocol
-
-__all__: list[str] = ["RepositoryProtocol"]
--- src/services/__init__.py
+++
@@ -1,6 +0,0 @@
-from src.services.chat import ChatService
-from src.services.file import FileService
-from src.services.invite import InviteService
-from src.services.message import MessageService
-
-__all__: list[str] = ["ChatService", "FileService", "InviteService", "MessageService"]
